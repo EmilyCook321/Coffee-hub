466a7d95ba15430ac1205ec275d29cf7f1c0e969
EmilyCook321 <emilyacook16@gmail.com>
2020-07-29 19:12:18 +0200

    Changes to details and quantitySelector  page

--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -1,27 +1,25 @@
 import "./details.css";
+import { createButton } from "../../components/button/button";
+import { createQuantitySelector } from "../../components/quantitySelector/quantitySelector";
 import { createElement } from "../../utils/elements";
+import macchiatoSrc from "../../assets/macciato.svg";
 
-export default {
-  title: "Pages/Details",
-};
+export default { title: "Pages/Details" };
 
 export const basic = () => {
   const main = createElement("main", {
     className: "details",
   });
-//creations/components
+  //creations/components
   const coffeeTitle = createElement("h2", {
     className: "details__title",
     innerText: "Macchiato",
   });
 
-  const quantitySelector = createElement("")
+  const quantitySelector = createElement("");
 
-  
   //relationships//
   main.append(coffeeTitle);
-
-
 
   return main;
 };
