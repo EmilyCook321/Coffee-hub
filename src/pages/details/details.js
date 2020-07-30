2430cab18c0d1835b988280d9e0e8153226d6c0b
EmilyCook321 <emilyacook16@gmail.com>
2020-07-30 06:49:45 +0200

    Add details.js page

---
+++ src/pages/details/details.js
@@ -0,0 +1,14 @@
+import "./details.css";
+import macchiatoSrc from "../../assets/macciato.svg";
+import { createButton } from "../../components/button/button";
+import { createQuantitySelector } from "../../components/quantitySelector/quantitySelector";
+import { createElement } from "../../utils/elements";
+//add this component!!
+import { createSizeSelector } from "../../components/sizeSelector";
+export default {
+
+    const createForm = () => {
+        const coffeName = createElement("div", { innerText: "Macchiato"});
+        const price = createElement("div", { innerText : "$2.80"});
+        coffeName.append(price);
+    }
